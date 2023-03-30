d882bafee03ee3b3401f5258dd0a537c223fd4b8
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-03-30 21:29:27 +0300

    Task_3 completed

---
+++ Seminar_1_Task_3.java
@@ -0,0 +1,38 @@
+// Семинар 1.
+// Задача № 3.
+// Реализовать простой калькулятор (+ - / *)
+// Ввод числа ->
+// Ввод знака ->
+// Ввод числа ->
+
+import java.util.Scanner;
+
+public class Seminar_1_Task_3 {
+    public static void main(String[] args) {
+        System.out.print("Введите число 1: ");
+        Scanner sc = new Scanner(System.in);
+        double number1 = sc.nextDouble();
+        System.out.print("Введите число 2: ");
+        double number2 = sc.nextDouble();
+        System.out.print("Знак: ");
+        String sign = sc.next();
+        char s = sign.charAt(0);
+        if(s == '+'){
+            double result = number1 + number2;
+            System.out.printf("%.2f", result);
+        }
+        if(s == '-'){
+            double result = number1 - number2;
+            System.out.printf("%.2f", result);
+        }
+        if(s == '/'){
+            double result = number1 / number2;
+            System.out.printf("%.2f", result);
+        }
+        if(s == '*'){
+            double result = number1 * number2;
+            System.out.printf("%.2f", result);
+        }
+        sc.close();
+    }
+}
