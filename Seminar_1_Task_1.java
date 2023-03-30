5ed3ce6494af490ff0c59dff60d18d691b6f0017
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-03-30 20:21:04 +0300

    add Task_1

---
+++ Seminar_1_Task_1.java
@@ -0,0 +1,19 @@
+// Семинар 1
+// Задача 1
+// Вычислить n-ое треугольного число(сумма чисел от 1 до n), 
+// а так же n! (произведение чисел от 1 до n)
+// Ввод:5
+// Треугольное число 1 + 2 + 3 + 4 + 5 = 15
+// n! 1 * 2 * 3 * 4 * 5 = 120
+import java.util.Scanner;
+
+public class Seminar_1_Task_1 {
+
+    public static void main(String[] args) {
+        System.out.print("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        
+        sc.close();
+    }
+}
