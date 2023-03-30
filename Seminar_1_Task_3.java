e38c381e94d723d18ae7d295ce841af2698e73d7
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-03-30 21:33:16 +0300

    Task_3 completed

--- Seminar_1_Task_3.java
+++ Seminar_1_Task_3.java
@@ -12,11 +12,12 @@ public class Seminar_1_Task_3 {
         System.out.print("Введите число 1: ");
         Scanner sc = new Scanner(System.in);
         double number1 = sc.nextDouble();
-        System.out.print("Введите число 2: ");
-        double number2 = sc.nextDouble();
         System.out.print("Знак: ");
         String sign = sc.next();
         char s = sign.charAt(0);
+        System.out.print("Введите число 2: ");
+        double number2 = sc.nextDouble();
+
         if(s == '+'){
             double result = number1 + number2;
             System.out.printf("%.2f", result);
