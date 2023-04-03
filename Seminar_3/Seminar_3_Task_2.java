13666c0c1784c329b273ec6c362685244f16cbf4
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-04-03 11:26:46 +0300

    completed Task_2 and Task_3

--- Seminar_3/Seminar_3_Task_2.java
+++ Seminar_3/Seminar_3_Task_2.java
@@ -14,7 +14,7 @@ public class Seminar_3_Task_2 {
         ArrayList<Integer> list = new ArrayList<>(10);
         Random rn = new Random();
         for (int i = 0; i < 10; i++) {
-            list.add(rn.nextInt(1, 100));
+            list.add(rn.nextInt(0, 100));
         }
         list.sort(Comparator.naturalOrder());
         System.out.println(list);
--- Seminar_3/Seminar_3_Task_3.java
+++ Seminar_3/Seminar_3_Task_3.java
@@ -1,5 +1,10 @@
 package Seminar_3;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.List;
+
 public class Seminar_3_Task_3 {
 
     // Семинар 3.
@@ -8,6 +13,36 @@ public class Seminar_3_Task_3 {
     // и среднее ариф. из этого списка.
 
     public static void main(String[] args) {
-        
+        ArrayList<Integer> list = new ArrayList<>(10);
+        Random rn = new Random();
+        for (int i = 0; i < 10; i++) {
+            list.add(rn.nextInt(0, 100));
+        }
+
+        System.out.print("Исходый список: ");
+        System.out.println(list);
+
+        list.sort(Comparator.naturalOrder());
+
+        int min = list.get(0);
+        System.out.print("Минимальное значение - ");
+        System.out.println(min);
+
+        int max = list.get(list.size() - 1);
+        System.out.print("Максимальное значение - ");
+        System.out.println(max);
+
+        double avg = getAverage(list);
+        System.out.print("Среднее арифмитическое значеие - ");
+		System.out.println(avg);
+
+    }
+
+    private static double getAverage(List<Integer> list) {
+		long sum = 0;
+		for (int i: list) {
+			sum += i;
+		}
+		return list.size() > 0 ? (double) sum / list.size() : 0;
     }
 }
